a46dc20004c8848e026c0da4abfae1487f82f7ef
Mederic Roy Beldia <twocngdagz@yahoo.com>
2025-10-03 21:37:42 +1000

    Scaffold vue 3 spa

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 import './bootstrap';
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import App from './App.vue';
+import LoginView from './Pages/LoginView.vue';
+import Dashboard from './Pages/Dashboard.vue';
+
+const routes = [
+    { path: '/', name: 'login', component: LoginView },
+    { path: '/dashboard', name: 'dashboard', component: Dashboard },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+createApp(App).use(router).mount('#app');
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+    content: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
+
